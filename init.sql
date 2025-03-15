e904dea363ed3a0e83e86e2290a8714e950a7fbb
Daniel-derf <danielpardinho321@gmail.com>
2025-03-15 16:33:30 -0300

    feat: users psql repository

--- init.sql
+++ init.sql
@@ -27,4 +27,35 @@ create table store.users (
   profile VARCHAR(10) CHECK (profile IN ('admin', 'client'))
 );
 
+create table store.wishlist (
+  id SERIAL PRIMARY KEY,
+  "userId" INT NOT NULL,
+  "productsIds" INT[]
+);
+
+-- initial population
+-- users
+insert into store.users (id, name, password, email, profile) values
+(1, 'testUser', '$2b$10$20IJmuLaI1P.duklED9Whu6QmkO2vgy.70K7mxZzZkmNvAh38YjAq', 'teste@email.com', 'client'),
+(2, 'testUser2', '$2b$10$20IJmuLaI1P.duklED9Whu6QmkO2vgy.70K7mxZzZkmNvAh38YjAq', 'teste2@email.com', 'client'),
+(3, 'testUser3', '$2b$10$20IJmuLaI1P.duklED9Whu6QmkO2vgy.70K7mxZzZkmNvAh38YjAq', 'teste3@email.com', 'client'),
+(4, 'testUser4', '$2b$10$20IJmuLaI1P.duklED9Whu6QmkO2vgy.70K7mxZzZkmNvAh38YjAq', 'teste4@email.com', 'admin');
+
+-- products
+insert into store.products (id, name, price, sex, description, "ratingId", "availableSizeQtt") values
+(1, 'Product 1', 100, 'M', 'Description 1', 1, 5),
+(2, 'Product 2', 200, 'F', 'Description 2', 2, 3),
+(3, 'Product 3', 300, 'M', 'Description 3', 3, 2),
+(4, 'Product 4', 400, 'F', 'Description 4', 4, 4),
+(5, 'Product 5', 500, 'M', 'Description 5', 5, 1);
+
+-- orders
+insert into store.orders (id, "userId", "productsIds", "totalPrice", status) values
+(1, 1, '{201, 202}', 100.5, 'CANCELLED'),
+(2, 1, '{203}', 50.0, 'TRANSPORT'),
+(3, 2, '{204, 205, 206}', 150.75, 'PREPARATION'),
+(4, 4, '{204, 205, 206}', 150.75, 'PREPARATION');
+
+
+
 
---
+++ src/users/repository/users.postgres.repository.ts
@@ -0,0 +1,49 @@
+import User from '../entities/user.entity';
+import IUsersRepository from './users.interface.repository';
+import * as pgPromise from 'pg-promise';
+
+const PSQL_URL =
+  process.env.PSQL_URL ?? 'postgres://user:123456@localhost:5432/store';
+
+const pgp = pgPromise();
+const connection = pgp(PSQL_URL);
+
+export default class UsersPostgresRepository implements IUsersRepository {
+  async findAll(): Promise<User[]> {
+    const usersData = await connection.query('select * from store.users');
+
+    return usersData.map((user) => new User(user));
+  }
+
+  async findById(id: number): Promise<User> {
+    const [userData] = await connection.query(
+      'select * from store.users u where u.id=$1',
+      [id],
+    );
+
+    return new User(userData);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const [userData] = await connection.query(
+      'select * from store.users u where u.email=$1',
+      [email],
+    );
+
+    return new User(userData);
+  }
+
+  async save(user: User): Promise<void> {
+    if (user.id === 0) {
+      await connection.query(
+        'insert into store.users (name, password, email, profile) values ($1, $2, $3, $4)',
+        [user.name, user.password, user.email, user.profile],
+      );
+    } else {
+      await connection.query(
+        'update store.users u set u.name=$1, u.password=$2, u.email=$3, u.profile=$4',
+        [user.name, user.password, user.email, user.profile],
+      );
+    }
+  }
+}
