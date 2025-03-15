7cd3b5060a365c6f3483eac13ae07d99e2a07943
Daniel-derf <danielpardinho321@gmail.com>
2025-03-15 16:07:01 -0300

    feat: products psql repository

--- init.sql
+++ init.sql
@@ -8,3 +8,23 @@ create table store.orders (
   "totalPrice" NUMERIC(10,2),
   status VARCHAR(30)
 );
+
+create table store.products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  price NUMERIC(10,2),
+  sex CHAR(1),
+  description TEXT,
+  "ratingId" INT,
+  "availableSizeQtt" INT
+);
+
+create table store.users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  profile VARCHAR(10) CHECK (profile IN ('admin', 'client'))
+);
+
+
---
+++ src/products/repository/products.postgres.repository.ts
@@ -0,0 +1,64 @@
+import { Product } from '../entities/product.entity';
+import IProductsRepository from './products.interface.repository';
+
+import * as pgPromise from 'pg-promise';
+
+const PSQL_URL =
+  process.env.PSQL_URL ?? 'postgres://user:123456@localhost:5432/store';
+
+const pgp = pgPromise();
+const connection = pgp(PSQL_URL);
+
+export default class ProductsPostgresRepository implements IProductsRepository {
+  async findAll(): Promise<Product[]> {
+    const productsData: any[] = await connection.query(
+      'SELECT * from store.products',
+    );
+
+    return productsData.map((product) => new Product(product));
+  }
+
+  async findById(id: number): Promise<Product> {
+    const [productData]: any[] = await connection.query(
+      'select * from store.products p where p.id = $1',
+      [id],
+    );
+
+    return new Product(productData);
+  }
+
+  async save(product: Product): Promise<void> {
+    if (product.id === 0) {
+      await connection.query(
+        'insert into store.products p (p.name, p.price, p.sex, p.description, p."ratingId", p."availableSizeQtt") values ($1, $2, $3, $4, $5, $6)',
+        [
+          product.name,
+          product.price,
+          product.sex,
+          product.description,
+          product.ratingId,
+          product.availableSizeQtt,
+        ],
+      );
+    } else {
+      await connection.query(
+        'update store.products set name=$1, price=$2, sex=$3, description=$4, "ratingId"=$5, "availableSizeQtt" = $6 where id=$7',
+        [
+          product.name,
+          product.price,
+          product.sex,
+          product.description,
+          product.ratingId,
+          product.availableSizeQtt,
+          product.id,
+        ],
+      );
+    }
+  }
+
+  async delete(productId: number): Promise<void> {
+    await connection.query('delete from store.products p where p.id=$1', [
+      productId,
+    ]);
+  }
+}
