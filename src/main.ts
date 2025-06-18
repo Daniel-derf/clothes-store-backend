bfc0e07d07f6a9fd8bbc377570d24376a1adda58
Daniel-derf <danielpardinho321@gmail.com>
2025-06-18 20:18:22 -0300

    feat: swagger initial setup

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Clothes Store API Documentation')
+    .setDescription('Swagger docs for Clothes Store API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 
